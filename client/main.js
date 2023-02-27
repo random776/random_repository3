e40b4404ee96462d1009309303cdfb21125cfc83
random776 <kazuki.tdn@gmail.com>
2023-02-27 09:47:30 +0900

    init

---
+++ client/main.js
@@ -0,0 +1,24 @@
+const messageList = document.getElementById("message-list");
+const messageInput = document.getElementById("message-input");
+
+document.getElementById("send-button").onclick = async () => {
+    const message = messageInput.value;
+    await fetch("/send", {
+        method: "post",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({message: message}),
+    });
+    //入力したものをjsonにしてリクエスト
+};
+
+setInterval(async () => {
+    const response = await fetch("/messages");
+    const messages = await response.json();
+    messageList.innerHTML = "";
+
+    for (const message of messages) {
+        const li = document.createElement("li");
+        li.textContent = message;
+        messageList.appendChild(li);
+    }
+}, 1000);
---
+++ server/main.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express(C);
+
+app.use(express.json());
+//Viteによって出力されたディレクトリの配信
+app.use(express.static("dist"));
+
+const messages = [];
+
+app.get("/messages", (request, response) => {
+    response.json(messages);
+});     //messagesをjsonデータとしてリクエストする
+
+app.post("/send", (request, response) => {
+    messages.push(request.body.message);
+    response.send();
+});     //messageにレスポンスされたものを突っ込む
+
+app.listen(3000);
